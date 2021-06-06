731f490927fa0208dd6fb44bb7b58e3b6315a82e
harindukodi <harindukodi@outlook.com>
2021-06-06 20:43:38 +1000

    Modified static

--- traceXapp/settings.py
+++ traceXapp/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print('Harindu BASE')
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -121,5 +122,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #
 # ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'static'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
