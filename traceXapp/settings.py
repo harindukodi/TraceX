bedf1d8d353793887e87b6a7535cede7e56a3645
harindukodi <harindukodi@outlook.com>
2021-06-07 13:45:40 +1000

    changes for RDS

--- traceXapp/settings.py
+++ traceXapp/settings.py
@@ -74,38 +74,48 @@ WSGI_APPLICATION = 'traceXapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'harindu',
+        'PASSWORD': 'harindukodituwakku',
+        'HOST': 'aa14wgrbiob9l8q.cbolvjxtjubd.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
-        # 'default': {
-        #     'ENGINE': 'django.db.backends.sqlite3',
-        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # }
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'postgres',
-            'USER': 'harindu',
-            'PASSWORD': 'harindukodituwakku',
-            'HOST': 'aa14wgrbiob9l8q.cbolvjxtjubd.us-east-1.rds.amazonaws.com',
-            'PORT': '5432',
-        }
-    }
+# if 'RDS_HOSTNAME' in os.environ:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.environ['RDS_DB_NAME'],
+#             'USER': os.environ['RDS_USERNAME'],
+#             'PASSWORD': os.environ['RDS_PASSWORD'],
+#             'HOST': os.environ['RDS_HOSTNAME'],
+#             'PORT': os.environ['RDS_PORT'],
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         # 'default': {
+#         #     'ENGINE': 'django.db.backends.sqlite3',
+#         #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         # }
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'postgres',
+#             'USER': 'harindu',
+#             'PASSWORD': 'harindukodituwakku',
+#             'HOST': 'aa14wgrbiob9l8q.cbolvjxtjubd.us-east-1.rds.amazonaws.com',
+#             'PORT': '5432',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
